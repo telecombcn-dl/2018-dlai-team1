7fd1fb23454fee1cebad727978e8985192ca54f6
Dani Fojo <dani.fojo@gmail.com>
2018-12-09 12:39:27 +0100

    Fixed pagan net

--- pytorch/pagan.py
+++ pytorch/pagan.py
@@ -20,6 +20,75 @@ import torch.nn.functional as F
 
 from utils import compute_fid
 
+
+# class Generator(nn.Module):
+#     def __init__(self, ngpu, nc, nz, ngf):
+#         super(Generator, self).__init__()
+#         self.ngpu = ngpu
+#         self.main = nn.Sequential(
+#             # input is Z, going into a convolution
+#             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+#             nn.BatchNorm2d(ngf * 8),
+#             nn.ReLU(True),
+#             # state size. (ngf*8) x 4 x 4
+#             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf * 4),
+#             nn.ReLU(True),
+#             # state size. (ngf*4) x 8 x 8
+#             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf * 2),
+#             nn.ReLU(True),
+#             # state size. (ngf*2) x 16 x 16
+#             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ngf),
+#             nn.ReLU(True),
+#             # state size. (ngf) x 32 x 32
+#             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+#             nn.Tanh()
+#             # state size. (nc) x 64 x 64
+#         )
+
+#     def forward(self, input):
+#         if input.is_cuda and self.ngpu > 1:
+#             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+#         else:
+#             output = self.main(input)
+#         return output
+
+
+# class Discriminator(nn.Module):
+#     def __init__(self, ngpu, nc, ndf):
+#         super(Discriminator, self).__init__()
+#         self.ngpu = ngpu
+#         self.main = nn.Sequential(
+#             # input is (nc) x 64 x 64
+#             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf) x 32 x 32
+#             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 2),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*2) x 16 x 16
+#             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 4),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*4) x 8 x 8
+#             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(ndf * 8),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             # state size. (ndf*8) x 4 x 4
+#             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+#             nn.Sigmoid(),
+#         )
+
+#     def forward(self, input):
+#         if input.is_cuda and self.ngpu > 1:
+#             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+#         else:
+#             output = self.main(input)
+
+#         return output.view(-1, 1).squeeze(1)
+
 class Generator(nn.Module):
     def __init__(self, ngpu, nc, nz, ngf):
         super(Generator, self).__init__()
@@ -83,8 +152,8 @@ class Discriminator(nn.Module):
                     ("conv6", spectral_norm(nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False))),
                     ("lrelu6", nn.LeakyReLU(0.2, inplace=True)),
                     # state size. (ndf*8) x 8 x 8
-                    ("conv6", spectral_norm(nn.Conv2d(ndf * 8, ndf * 8, 3, 1, 1, bias=False))),
-                    ("lrelu6", nn.LeakyReLU(0.2, inplace=True)),
+                    ("conv7", spectral_norm(nn.Conv2d(ndf * 8, ndf * 8, 3, 1, 1, bias=False))),
+                    ("lrelu7", nn.LeakyReLU(0.2, inplace=True)),
                     # state size. (ndf*8) x 8 x 8
                 ]
             )
@@ -100,7 +169,7 @@ class Discriminator(nn.Module):
             x = x.view(-1, (self.ndf*8) * 8 * 8)
             output = torch.sigmoid(self.fc(x))
 
-        return output
+        return output.squeeze()
 
 
 # custom weights initialization called on netG and netD
