ec8824b437a98dc699d8af24c001b15f5f0883bc
Dani Fojo <dani.fojo@gmail.com>
2018-12-10 20:34:46 +0100

    Changed tensorflow code to another branch

--- utils.py
+++ utils.py
@@ -1,250 +1,177 @@
-"""
-Some codes from https://github.com/Newmu/dcgan_code
-"""
-from __future__ import division
-import math
-import json
-import random
-import pprint
-import scipy.misc
+import torch
+import torch.nn.functional as F
+import torchvision
 import numpy as np
-from time import gmtime, strftime
-from six.moves import xrange
+from inception import InceptionV3
+from torch.utils.data import TensorDataset, DataLoader
+from torch.nn.functional import adaptive_avg_pool2d
+from tqdm import tqdm
+from scipy import linalg
 
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
+model = InceptionV3()
+model.eval()
 
-pp = pprint.PrettyPrinter()
-
-get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
-
-def show_all_variables():
-  model_vars = tf.trainable_variables()
-  slim.model_analyzer.analyze_vars(model_vars, print_info=True)
-
-def get_image(image_path, input_height, input_width,
-              resize_height=64, resize_width=64,
-              crop=True, grayscale=False):
-  image = imread(image_path, grayscale)
-  return transform(image, input_height, input_width,
-                   resize_height, resize_width, crop)
-
-def save_images(images, size, image_path):
-  return imsave(inverse_transform(images), size, image_path)
-
-def imread(path, grayscale = False):
-  if (grayscale):
-    return scipy.misc.imread(path, flatten = True).astype(np.float)
-  else:
-    return scipy.misc.imread(path).astype(np.float)
-
-def merge_images(images, size):
-  return inverse_transform(images)
-
-def merge(images, size):
-  h, w = images.shape[1], images.shape[2]
-  if (images.shape[3] in (3,4)):
-    c = images.shape[3]
-    img = np.zeros((h * size[0], w * size[1], c))
-    for idx, image in enumerate(images):
-      i = idx % size[1]
-      j = idx // size[1]
-      img[j * h:j * h + h, i * w:i * w + w, :] = image
-    return img
-  elif images.shape[3]==1:
-    img = np.zeros((h * size[0], w * size[1]))
-    for idx, image in enumerate(images):
-      i = idx % size[1]
-      j = idx // size[1]
-      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
-    return img
-  else:
-    raise ValueError('in merge(images,size) images parameter '
-                     'must have dimensions: HxW or HxWx3 or HxWx4')
-
-def imsave(images, size, path):
-  image = np.squeeze(merge(images, size))
-  return scipy.misc.imsave(path, image)
-
-def center_crop(x, crop_h, crop_w,
-                resize_h=64, resize_w=64):
-  if crop_w is None:
-    crop_w = crop_h
-  h, w = x.shape[:2]
-  j = int(round((h - crop_h)/2.))
-  i = int(round((w - crop_w)/2.))
-  return scipy.misc.imresize(
-      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
-
-def transform(image, input_height, input_width, 
-              resize_height=64, resize_width=64, crop=True):
-  if crop:
-    cropped_image = center_crop(
-      image, input_height, input_width, 
-      resize_height, resize_width)
-  else:
-    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
-  return np.array(cropped_image)/127.5 - 1.
-
-def inverse_transform(images):
-  return (images+1.)/2.
-
-def to_json(output_path, *layers):
-  with open(output_path, "w") as layer_f:
-    lines = ""
-    for w, b, bn in layers:
-      layer_idx = w.name.split('/')[0].split('h')[1]
-
-      B = b.eval()
-
-      if "lin/" in w.name:
-        W = w.eval()
-        depth = W.shape[1]
-      else:
-        W = np.rollaxis(w.eval(), 2, 0)
-        depth = W.shape[0]
-
-      biases = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(B)]}
-      if bn != None:
-        gamma = bn.gamma.eval()
-        beta = bn.beta.eval()
-
-        gamma = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(gamma)]}
-        beta = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(beta)]}
-      else:
-        gamma = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-        beta = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-
-      if "lin/" in w.name:
-        fs = []
-        for w in W.T:
-          fs.append({"sy": 1, "sx": 1, "depth": W.shape[0], "w": ['%.2f' % elem for elem in list(w)]})
-
-        lines += """
-          var layer_%s = {
-            "layer_type": "fc", 
-            "sy": 1, "sx": 1, 
-            "out_sx": 1, "out_sy": 1,
-            "stride": 1, "pad": 0,
-            "out_depth": %s, "in_depth": %s,
-            "biases": %s,
-            "gamma": %s,
-            "beta": %s,
-            "filters": %s
-          };""" % (layer_idx.split('_')[0], W.shape[1], W.shape[0], biases, gamma, beta, fs)
-      else:
-        fs = []
-        for w_ in W:
-          fs.append({"sy": 5, "sx": 5, "depth": W.shape[3], "w": ['%.2f' % elem for elem in list(w_.flatten())]})
-
-        lines += """
-          var layer_%s = {
-            "layer_type": "deconv", 
-            "sy": 5, "sx": 5,
-            "out_sx": %s, "out_sy": %s,
-            "stride": 2, "pad": 1,
-            "out_depth": %s, "in_depth": %s,
-            "biases": %s,
-            "gamma": %s,
-            "beta": %s,
-            "filters": %s
-          };""" % (layer_idx, 2**(int(layer_idx)+2), 2**(int(layer_idx)+2),
-               W.shape[0], W.shape[3], biases, gamma, beta, fs)
-    layer_f.write(" ".join(lines.replace("'","").split()))
-
-def make_gif(images, fname, duration=2, true_image=False):
-  import moviepy.editor as mpy
-
-  def make_frame(t):
-    try:
-      x = images[int(len(images)/duration*t)]
-    except:
-      x = images[-1]
-
-    if true_image:
-      return x.astype(np.uint8)
+def add_channel(images, augmentation_bits, real):
+    if augmentation_bits is None:
+        labels = (
+            torch.ones(size=(images.size(0),), device=images.device)
+            if real
+            else torch.zeros(size=(images.size(0),), device=images.device)
+        )
+        return images, labels
     else:
-      return ((x+1)/2*255).astype(np.uint8)
+        #XOR
+        print('augmentation bits: ', augmentation_bits)
+        labels = torch.Tensor((np.sum(augmentation_bits.numpy(), axis=1) + real )% 2)
+        print('labels: ', labels)
+        #Add channels
+        augmentation_level = augmentation_bits.shape[1]
+        batch_size = augmentation_bits.shape[0]
+        a = np.empty((batch_size, augmentation_level, 64, 64))
 
-  clip = mpy.VideoClip(make_frame, duration=duration)
-  clip.write_gif(fname, fps = len(images) / duration)
+        for i in range(batch_size):
+            for j in range (augmentation_level):
+                a[i, j, :, :] = augmentation_bits[i,j]
 
-def visualize(sess, dcgan, config, option):
-  image_frame_dim = int(math.ceil(config.batch_size**.5))
-  if option == 0:
-    z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
-    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-    save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
-  elif option == 1:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(dcgan.z_dim):
-      print(" [*] %d" % idx)
-      z_sample = np.random.uniform(-1, 1, size=(config.batch_size , dcgan.z_dim))
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
+        print('image shape:', images.size())
+        print('tensor a shape: ', torch.Tensor(a).size())
+        images = torch.cat((images, torch.Tensor(a)) ,dim=1)
 
-      if config.dataset == "mnist":
-        y = np.random.choice(10, config.batch_size)
-        y_one_hot = np.zeros((config.batch_size, 10))
-        y_one_hot[np.arange(config.batch_size), y] = 1
-
-        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
-      else:
-        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-
-      save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
-  elif option == 2:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in [random.randint(0, dcgan.z_dim - 1) for _ in xrange(dcgan.z_dim)]:
-      print(" [*] %d" % idx)
-      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
-      z_sample = np.tile(z, (config.batch_size, 1))
-      #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      if config.dataset == "mnist":
-        y = np.random.choice(10, config.batch_size)
-        y_one_hot = np.zeros((config.batch_size, 10))
-        y_one_hot[np.arange(config.batch_size), y] = 1
-
-        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
-      else:
-        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-
-      try:
-        make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-      except:
-        save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
-  elif option == 3:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(dcgan.z_dim):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-  elif option == 4:
-    image_set = []
-    values = np.arange(0, 1, 1./config.batch_size)
-
-    for idx in xrange(dcgan.z_dim):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
-
-      image_set.append(sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample}))
-      make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
-
-    new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
-        for idx in range(64) + range(63, -1, -1)]
-    make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
+        return images, labels
 
 
-def image_manifold_size(num_images):
-  manifold_h = int(np.floor(np.sqrt(num_images)))
-  manifold_w = int(np.ceil(np.sqrt(num_images)))
-  assert manifold_h * manifold_w == num_images
-  return manifold_h, manifold_w
+def compute_kid(real, fake, batch_size=128):
+    model.to(real.device)
+    print("Computing Inception Activations")
+    real_activations = get_activations(real, model, batch_size)
+    fake_activations = get_activations(fake, model, batch_size)
+    print("Computing KID")
+    return _kid(real_activations, fake_activations)
+
+
+def compute_fid(real, fake, batch_size=128):
+    model.to(real.device)
+    real_activations = get_activations(real, model, batch_size)
+    real_mu = np.mean(real_activations, axis=0)
+    real_sigma = np.cov(real_activations, rowvar=False)
+
+    fake_activations = get_activations(fake, model, batch_size)
+    fake_mu = np.mean(fake_activations, axis=0)
+    fake_sigma = np.cov(fake_activations, rowvar=False)
+    return _fid(real_mu, real_sigma, fake_mu, fake_sigma)
+
+
+def get_activations(images, model, batch_size=128, dims=2048):
+    """Calculates the activations of the pool_3 layer for all images.
+    Params:
+    -- images      : Numpy array of dimension (n_images, 3, hi, wi). The values
+                     must lie between 0 and 1.
+    -- model       : Instance of inception model
+    -- batch_size  : the images numpy array is split into batches with
+                     batch size batch_size. A reasonable batch size depends
+                     on the hardware.
+    -- dims        : Dimensionality of features returned by Inception
+    Returns:
+    -- A numpy array of dimension (num images, dims) that contains the
+       activations of the given tensor when feeding inception with the
+       query tensor.
+    """
+
+    loader = DataLoader(images, batch_size=batch_size)
+    preds = []
+    with torch.no_grad():
+        for batch in tqdm(loader, desc="Computing inception activations"):
+            pred = model(batch)[0]
+            # If model output is not scalar, apply global spatial average pooling.
+            # This happens if you choose a dimensionality not equal 2048.
+            if pred.shape[2] != 1 or pred.shape[3] != 1:
+                pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+            
+            preds.append(pred)
+        preds = torch.cat(preds, dim=0).squeeze().cpu().detach().numpy()
+    return preds
+
+def _kid(X, Y):
+    """
+    Given X, Y (numpy) batches of inception outputs of generated and real images,
+    return the Kernel Inception Distance. X and Y have to have the same dimensions.
+    """
+    assert np.all(X.shape == Y.shape)
+
+    n = X.shape[0]
+
+    def k(x, y):
+        # Kernel
+        return (1/x.shape[0]*np.dot(x, y)+1)**(1/3)
+
+    def f(X, Y):
+        # First 2 sums. We use the fact that k is symmetric
+        res = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                res += k(X[i], Y[j])
+        
+        return 2*res
+    
+    def f2(X, Y):
+        # Third sum.
+        res = f(X, Y)
+
+        for i in range(n):
+            res += k(X[i], Y[i])
+        
+        return res
+
+    return 1/(n*(n-1))*f(X, X) + 1/(n*(n-1))*f(Y, Y) - 2/(n**2)*f2(X, Y)
+
+
+def _fid(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    Stable version by Dougal J. Sutherland.
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an 
+               representive data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an 
+               representive data set.
+    Returns:
+    --   : The Frechet Distance.
+    """
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
